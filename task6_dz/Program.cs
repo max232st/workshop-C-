a1d76ec706dfeaf033f1edc6efe50869d46bc32f
max232st <max232st@gmail.com>
2022-10-04 18:01:48 +0300

    Домашнее задание

---
+++ task6_dz/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 78 5  -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число ");
+int number3 = int.Parse(Console.ReadLine());
+int max = 0;
+if (number1 > number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+
+if (max > number3)
+{
+    Console.WriteLine("max = " +max);
+}
+
+else 
+{
+    max = number3;
+    Console.WriteLine("max = " +max);
+}
---
+++ task8_dz/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+//int N = 2;
+
+if (number >= 2)
+{
+    int N = 2;
+    while (N <= number)
+    {
+        Console.Write(N);
+        Console.Write(", ");
+        N = N + 2;
+    }
+}
+else Console.Write("Не верно задано число");
