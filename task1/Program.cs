d60bab16179b9b2eb21924174e38f2ed5460c051
max232st <max232st@gmail.com>
2022-10-04 17:44:42 +0300

    Задачи на семинаре

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 25, b = 5 -> да 
+a = 2, b = 10 -> нет 
+a = 9, b = -3 -> да 
+a = -3 b = 9 -> нет
+Math.Pow(number,2) возведение в степень
+*/
+Console.WriteLine("Введите первое число");
+int numberOne = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberTwo = int.Parse(Console.ReadLine());
+
+if (numberOne == numberTwo*numberTwo) // Math.Pow(number,2) возведение в степень
+{
+    Console.WriteLine("Да");
+    
+}
+else
+    {
+        Console.WriteLine("Нет");
+    }
---
+++ task3/Program.cs
@@ -0,0 +1,40 @@
+﻿/*3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+	3 -> Среда 
+5 -> Пятница */
+
+Console.WriteLine("Введите номер для недели");
+int numberWeek = int.Parse(Console.ReadLine());
+ 
+if (numberWeek < 1 || numberWeek > 7)
+    {
+    Console.WriteLine("НЕКОРЕКТНЫЙ ВВОД");
+    }
+if (numberWeek == 1)
+{
+    Console.WriteLine("Monday");
+}
+if (numberWeek == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (numberWeek == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+if (numberWeek == 4)
+{
+    Console.WriteLine("Thirsday");
+}
+
+if (numberWeek == 5)
+{
+    Console.WriteLine("Fridai");
+}
+if (numberWeek == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if (numberWeek == 7)
+{
+    Console.WriteLine("Sunday");
+}
